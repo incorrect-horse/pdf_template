9f3ff36ee4be217fb55fef2a780540de5dabe2ea
incorrect-horse <vincenso_h@yahoo.com>
2023-01-03 17:09:12 -0500

    added footer text to pages

--- main.py
+++ main.py
@@ -2,17 +2,32 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="letter")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv", sep=",")
 
 for index, row in df.iterrows():
-    for i in range(row["Pages"]):
-        #print(i)
+    pdf.add_page()
+
+    #header
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+    pdf.line(10, 21, 200, 21)
+
+    #footer
+    pdf.ln(245)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
 
-        pdf.set_font(family="Times", style="B", size=24)
-        pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-        pdf.line(10, 21, 200, 21)
+        #footer
+        pdf.ln(257)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
